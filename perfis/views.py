b7b2dfa4ce16afc167b0c7768cb4eede8a77605d
Priscila Paula <poliveira13@hotmail.com.br>
2019-05-01 03:58:56 -0300

    autorizando e autenticando usuários, tela de login

--- perfis/views.py
+++ perfis/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from perfis.models import Perfil
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
 	return render(request, 'index.html')
 
+@login_required
 def exibir(request, perfil_id):
 
-	perfil = Perfil()
-	return render(request, 'perfil.html', {'perfil' :perfil, 'perfil_logado': get_perfil_logado(request)})
+	perfil = Perfil.objects.get(id=perfil_id)
+	perfil_logado = get_perfil_logado(request)
 
+	return render(request, 'perfil.html', {'perfil' : perfil, 'perfil_logado': get_perfil_logado(request)})
+
+@login_required
+def get_perfil_logado(request):
+	return request.user.perfil
 
 
 
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,5 +3,7 @@ from usuarios.views import CadastrarUsuario
 from usuarios.forms import CadastrarUserForm
 
 urlpatterns = patterns('',
-	url(r'^cadastrar/$', CadastrarUsuario.as_view(), name="cadastrar")
+	url(r'^cadastrar/$', CadastrarUsuario.as_view(), name="cadastrar"),
+	url(r'^login/$', 'django.contrib.auth.views.login', {'template_name' : 'login.html'}, name='login'),
+	url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', {'login_url' : '/login/' }, name='logout')
 )
